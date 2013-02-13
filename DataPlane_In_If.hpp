8c15f47d4435581717390beb393772bc9a48c3b6
Antti Siirilä <antipant@antipant-MacBookPro.(none)>
2013-02-13 15:52:16 +0200

    Input interface of data plane for BGP sessions

---
+++ DataPlane_In_If.hpp
@@ -0,0 +1,54 @@
+/*! \file  DataPlane_In_If.hpp.hpp
+ *  \brief   
+ *  \details   
+ *  \author    Antti Siirilä, 501449
+ *  \version   1.0
+ *  \date      Tue Feb 12 13:01:27 2013
+ */
+
+/*!
+ * \class DataPlane_In_If.hpp
+ * \brief
+ *  \details
+ */
+
+
+#include "systemc"
+#include "BGPMessage.hpp"
+
+using namespace std;
+using namespace sc_core;
+using namespace sc_dt;
+
+#ifndef _DATAPLANE_IN_IF_H_
+#define _DATAPLANE_IN_IF_H_
+
+
+
+
+class DataPlane_In_If.hpp: virtual public sc_interface
+{
+
+public:
+
+
+
+  /*! \brief Allows the session to pass BGP messages to the DataPlane
+   * \details The method shall implement a mutex that takes care of
+   * the arbitraton between sessions
+   * @param[in] BGPMessage p_BGPMsg  The BGP message to be send
+   * \return bool True: if success is valid, False: if not success
+   * \public
+   */
+    virtual bool write(BGPMessage p_BGPMsg) = 0;
+
+
+
+
+};
+
+
+#endif /* _DATAPLANE_IN_IF_H_ */
+
+
+
