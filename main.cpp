a329909f4ec0a9997b6c85b6d9b207fbee76d1f9
Antti Siirilä <antipant@antipant-MacBookPro.(none)>
2013-04-09 14:41:03 +0300

    Debugging messages turned off from the main

--- main.cpp
+++ main.cpp
@@ -118,7 +118,7 @@ int sc_main(int argc, char * argv [])
     sc_report_handler::set_log_file_name("test_simu.log");
     sc_report_handler::set_actions(g_ReportID, SC_INFO, SC_DISPLAY);
     sc_report_handler::set_actions(g_DebugID, SC_INFO, SC_DO_NOTHING);
-    sc_report_handler::set_actions(g_DebugMainID, SC_INFO, SC_DISPLAY);
+    sc_report_handler::set_actions(g_DebugMainID, SC_INFO, SC_DO_NOTHING);
     SC_REPORT_INFO(g_ReportID, g_SimulationVersion);
 
     StringTools l_DebugReport("Main");
