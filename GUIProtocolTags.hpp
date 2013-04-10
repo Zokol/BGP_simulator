de938f01d8fb8650086b94c5738fcd2992168baf
Antti Siirilä <antipant@antipant-MacBookPro.(none)>
2013-04-10 14:30:39 +0300

    Protocol commands updated

--- GUIProtocolTags.hpp
+++ GUIProtocolTags.hpp
@@ -47,24 +47,83 @@
 /*!
  * Positive acknowledgement
  */
-#define ACK "<akc />"
 
 /*!
  * Negative acknowledgement
  */
-#define NACK "<ack />"
+#define NACK "NACK"
 
-#define RESET_ROUTER "RESET_ROUTER"//RESET_ROUTER_N
-#define KILL_ROUTER "KILL_ROUTER" //KILL_ROUTER_N
-#define REVIVE_ROUTER "REVIVE_ROUTER" //REVIVE_ROUTER_N
-#define READ_ROUTER "READ_ROUTER" //READ_ROUTER_LIST
-#define READ_ROUTER "READ_ROUTER" //READ_ROUTER_N
-#define READ_PACKET "READ_PACKET" //READ_PACKET_N
-#define SEND_PACKET "SEND_PACKET" //SEND_PACKET PORT_N [HEADER] [PAYLOAD]
-#define CONNECT "CONNECT" //CONNECT PORT_N PORT_N
-#define DISCONNECT "DISCONNECT" //DISCONNECT PORT_N
-#define STOP "STOP"
-#define READ_TABLE "READ_TABLE" //READ_TABLE_ID
+
+/*!
+ * Empties the routing tables
+ */
+#define RESET_ROUTER "RESET_ROUTER"//RESET_ROUTER [ROUTER_ID] 
+
+/*!
+ * Diconnects all interfaces, which are up and empties the routing tables
+ */
+#define KILL_ROUTER "KILL_ROUTER" //KILL_ROUTER [ROUTER_ID] 
+
+/*!
+ * Recovers from the KILL_ROUTER. Sets the interfaces up.
+ */
+#define REVIVE_ROUTER "REVIVE_ROUTER" //REVIVE_ROUTER [ROUTER_ID]
+
+/*!
+ *Reads message string of received messages from the router.
+ */
+#define READ_PACKET "READ_PACKET" //READ_PACKET [ROUTER_ID]
+
+/*!
+ *Clears the message string of received messages
+ */
+#define CLEAR_PACKET "CLEAR_PACKET" //CLEAR_PACKET [ROUTER_ID]
+
+/*!
+ * Sends a message to the given AS prefix
+ */
+#define SEND_PACKET "SEND_PACKET" //SEND_PACKET [DEST_PREFIX] [ROUTER_ID] [PAYLOAD]
+
+/*!
+ * Set the interfaceup that is been DISCONNECTED
+ */
+#define CONNECT "CONNECT" //CONNECT ROUTER_ID  [PORT_ID]
+
+/*!
+ * Puts the interface down in the given router
+ */
+#define DISCONNECT "DISCONNECT" //DISCONNECT [ROUTER_ID] [PORT_ID]
+
+/*!
+ * Terminates the simulations
+ */
+#define STOP "STOP" //STOP
+
+/*!
+ * Reads the routing table from the given router
+ */
+#define READ_TABLE "READ_TABLE" //READ_TABLE [ROUTER_ID]
+
+/*!
+ * Reads the raw table with all learned routes from the given router
+ */
+#define READ_RAW_TABLE "READ_RAW_TABLE" //READ_RAW_TABLE [ROUTER_ID]
+
+/*!
+ * Set the local preference value for the give AS in the given router
+ */
+#define SET_LOCAL_PREF "SET_LOCAL_PREF" //SET_LOCAL_PREF [ROUTER_ID] [AS_ID] [VALUE]
+
+/*!
+ * Set the keepalive time for the given router 
+ */
+#define SET_KEEPALIVE "SET_KEEPALIVE" // SET_KEEPALIVE ROUTER_ID [ROUTE_ID] [VALUE]
+
+/*!
+ * Set the hold-down time multiplier for the given router
+ */
+#define SET_HOLDDOWN_MULT "SET_HOLDDOWN_MULT" // SET_HOLDDOWN_MULT [ROUTER_ID] [VALUE]
+
 
 
 ///************************ Program modes *******************************
