81aeafb367434b2316b2cf6bcf167eb2d7032d9a
Antti Siirilä <antipant@antipant-MacBookPro.(none)>
2013-04-25 12:52:00 +0300

    g_ErrorID added

--- ReportGlobals.hpp
+++ ReportGlobals.hpp
@@ -5,5 +5,5 @@ extern const char *g_ReportID;
 extern const char *g_DebugID;
 extern const char *g_DebugCPID;
 extern const char *g_DebugBSID;
-
+extern const char *g_ErrorID;
 #endif /* _REPORTGLOBALS_H_ */
